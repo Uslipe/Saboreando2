80fcaca2fc069ae8e25eb0ffbad4158326f65c69
Uslipe <felipe.pereira.nave@gmail.com>
2024-07-15 22:45:23 -0300

    feat: Método de cadastrar usuário no controlador

--- saboreando2/src/main/java/com/saboreando/negocio/ControladorUsuario.java
+++ saboreando2/src/main/java/com/saboreando/negocio/ControladorUsuario.java
@@ -10,8 +10,22 @@ public class ControladorUsuario {
         this.repositorioUsuario = RepositorioUsuario.getInstance();
     }
 
-    //
+    //Cadastrar usuário
     public void cadastrarUsuario(Usuario u){
-
+        //Verifica se todos os campos são preenchidos
+        if(u.getNome() == null || u.getEmail() == null || u.getUsername() == null || u.getSenha() == null){
+            throw new IllegalArgumentException("Argumento nulo");
+        }
+        //Caso todos estejam preenchidos...
+        else{
+            //...Verifica se o username já é cadastrado
+            if(repositorioUsuario.procurarUsuarioIndice(u.getUsername()) == -1){
+                repositorioUsuario.inserir(u);
+            }
+            else{
+                throw new IllegalArgumentException("Usuário existente");
+            }
+        }
     }
 }
+
