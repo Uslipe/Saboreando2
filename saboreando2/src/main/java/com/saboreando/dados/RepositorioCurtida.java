0311e7b7a99730bb2249121f1a5c26092aaa9a6f
Uslipe <felipe.pereira.nave@gmail.com>
2024-07-24 23:35:35 -0300

    feat: Repositório de curtidas adicionado

---
+++ saboreando2/src/main/java/com/saboreando/dados/RepositorioCurtida.java
@@ -0,0 +1,44 @@
+package com.saboreando.dados;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.saboreando.dados.beans.Comentario;
+import com.saboreando.dados.beans.Curtida;
+import com.saboreando.dados.beans.Postagem;
+
+public class RepositorioCurtida {
+    private static RepositorioCurtida instance;
+    private ArrayList<Curtida> listaCurtida;
+
+    //Padrão singleton
+    public static RepositorioCurtida getInstance(){
+        if(instance == null){
+            instance = new RepositorioCurtida();
+        }
+        return instance;
+    }
+
+    //Construtor
+    public RepositorioCurtida() {
+        this.listaCurtida = new ArrayList<>();
+    }
+
+    //Inserir curtida na lista
+    public void inserir(Curtida curtida){
+        listaCurtida.add(curtida);
+    }
+
+    //Retorna uma lista com todas as curtidas relacionadas a determinada postagem
+    public List<Curtida> listarCurtidasPostagem(Postagem postagem){
+        List<Curtida> curtidasDaPostagem = new ArrayList<>();
+        //Vasculha o array atrás de comentários
+        for(Curtida curtida : listaCurtida){
+            //Verificar se a postagem armazenada no comentário se iguala a postagem do parâmetro
+            if(curtida.getPostagemRelacionada().equals(postagem)){
+                curtidasDaPostagem.add(curtida);
+            }
+        }
+        return curtidasDaPostagem;
+    }
+}
--- saboreando2/src/main/java/com/saboreando/dados/beans/Curtida.java
+++ saboreando2/src/main/java/com/saboreando/dados/beans/Curtida.java
@@ -6,4 +6,13 @@ public class Curtida extends Interacao {
         super(autor, postagemRelacionada);
     }
     
+    //Obter autor da interação
+    public Usuario getAutor(Interacao interacao){
+        return interacao.getAutor();
+    }
+
+    // Método para obter a postagem relacionada de uma interação
+    public Postagem getPostagemRelacionada(Interacao interacao) {
+        return interacao.getPostagemRelacionada();
+    }
 }
