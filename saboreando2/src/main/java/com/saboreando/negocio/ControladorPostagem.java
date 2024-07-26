f6e83765c04c5483333237972f14e5976b59c951
Uslipe <felipe.pereira.nave@gmail.com>
2024-07-25 22:38:42 -0300

    feat: Criação do controlador postagem

---
+++ saboreando2/src/main/java/com/saboreando/negocio/ControladorPostagem.java
@@ -0,0 +1,28 @@
+package com.saboreando.negocio;
+
+import com.saboreando.dados.RepositorioPostagem;
+import com.saboreando.dados.RepositorioUsuario;
+import com.saboreando.dados.beans.Postagem;
+
+public class ControladorPostagem {
+    private static ControladorPostagem instance;
+    private RepositorioPostagem repositorioPostagem;
+
+    //Padrão singleton de única instância
+    public static ControladorPostagem getInstance(){
+        if(instance == null){
+            instance = new ControladorPostagem();
+        }
+        return instance;
+    }
+
+    //Construtor da classe
+    public ControladorPostagem(){
+        this.repositorioPostagem = RepositorioPostagem.getInstance();
+    }
+
+    //Criar uma nova postagem
+    public void criarPostagem(Postagem postagem){
+
+    }
+}
