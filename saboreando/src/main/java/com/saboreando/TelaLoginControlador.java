6bd86af121092895f8fa0c680328b8d5b0534021
Uslipe <felipe.pereira.nave@gmail.com>
2024-08-30 00:54:06 -0300

    feat: Design da tela de login

--- saboreando/src/main/java/com/saboreando/TelaLoginControlador.java
+++ saboreando/src/main/java/com/saboreando/TelaLoginControlador.java
@@ -1,10 +1,22 @@
 package com.saboreando;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class TelaLoginControlador {
     
+    @FXML
+    private Button botaoLogar;
+
+    @FXML
+    private PasswordField inputSenha;
+
+    @FXML
+    private TextField inputUsuario;
+
     @FXML
     private Label titulo;
 }
