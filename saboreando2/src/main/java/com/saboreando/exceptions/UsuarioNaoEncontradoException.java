847e488dd6ad0859ed7057038da33ea7ad348dc2
Felipe Reis <107627690+Uslipe@users.noreply.github.com>
2024-08-29 23:44:18 -0300

    Deletar arquivos antigos

--- saboreando2/src/main/java/com/saboreando/exceptions/UsuarioNaoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package com.saboreando.exceptions;
-
-public class UsuarioNaoEncontradoException extends Exception {
-
-    public UsuarioNaoEncontradoException(){
-        super("O usuário não foi encontrado");
-    }
-}
