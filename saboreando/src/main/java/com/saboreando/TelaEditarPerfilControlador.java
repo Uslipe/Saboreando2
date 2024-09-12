f57ffcef0d7a0c43f3f17c73b1d4d9cd03f2999c
Uslipe <felipe.pereira.nave@gmail.com>
2024-09-12 14:27:58 -0300

    feat: Base da tela de editar dados do perfil feita

--- saboreando/src/main/java/com/saboreando/TelaEditarPerfilControlador.java
+++ saboreando/src/main/java/com/saboreando/TelaEditarPerfilControlador.java
@@ -8,18 +8,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class TelaPerfilControlador {
+public class TelaEditarPerfilControlador {
 
     Fachada fachada = Fachada.getInstance();
 
     @FXML
-    private Button botaoEditarPerfil;
-    
+    private Button botaoEnviar;
+
     @FXML
     private HBox hboxCriar;
 
@@ -30,37 +30,39 @@ public class TelaPerfilControlador {
     private HBox hboxPerfil;
 
     @FXML
-    private Hyperlink linkParaTelaFeed;
+    private Hyperlink hyperLinkTelaFeed;
 
     @FXML
-    private Label nomeDisplay;
+    private Hyperlink hyperLinkTelaPerfil;
 
     @FXML
-    private Label numPublicacoes;
+    private TextField inputEditarEmail;
 
     @FXML
-    private Label usernameDisplay;
+    private TextField inputEditarNome;
+
+    @FXML
+    private TextField inputEditarUsername;
 
     @FXML
     private void initialize(){
-        nomeDisplay.setText(fachada.pegarInstanciaUsuarioLogado().getNome());
-        usernameDisplay.setText(fachada.pegarInstanciaUsuarioLogado().getUsername());
-        numPublicacoes.setText(String.valueOf(fachada.retornarQntPostagensUsuario(fachada.pegarInstanciaUsuarioLogado())));
+        botaoEnviar.setOnMouseEntered(event -> botaoEnviar.setStyle("-fx-background-color: #b30746; -fx-background-radius: 8"));
+        botaoEnviar.setOnMouseExited(event -> botaoEnviar.setStyle("-fx-background-color: #e00958;  -fx-background-radius: 8"));
 
-        botaoEditarPerfil.setOnMouseEntered(event -> botaoEditarPerfil.setStyle("-fx-background-color: #b30746; -fx-background-radius: 8"));
-        botaoEditarPerfil.setOnMouseExited(event -> botaoEditarPerfil.setStyle("-fx-background-color: #e00958;  -fx-background-radius: 8"));
+        inputEditarEmail.setText(fachada.pegarInstanciaUsuarioLogado().getEmail());
+        inputEditarNome.setText(fachada.pegarInstanciaUsuarioLogado().getNome());
+        inputEditarUsername.setText(fachada.pegarInstanciaUsuarioLogado().getUsername());
     }
 
-    //Direcionamento para tela feed
     @FXML
-    public void HyperlinkActionTelaFeed(@SuppressWarnings("exports") ActionEvent event) {
+    public void HyperLinkActionTelaFeed(@SuppressWarnings("exports") ActionEvent event) {
         try {
             // Carregar o novo arquivo FXML
             FXMLLoader loader = new FXMLLoader(TelaLoginControlador.class.getResource("telaFeed.fxml"));
             AnchorPane novaTela = loader.load();
             
             // Obter a cena atual e definir a nova tela como seu conteúdo
-            Stage stage = (Stage) linkParaTelaFeed.getScene().getWindow();
+            Stage stage = (Stage) hyperLinkTelaFeed.getScene().getWindow();
             Scene novaCena = new Scene(novaTela);
             stage.setScene(novaCena);
 
@@ -69,16 +71,15 @@ public class TelaPerfilControlador {
         }
     }
 
-    //Direcionamento para tela editar perfil
     @FXML
-    public void HyperlinkActionTelaEditarPerfil(@SuppressWarnings("exports") ActionEvent event) {
+    public void HyperLinkActionTelaPerfil(@SuppressWarnings("exports") ActionEvent event) {
         try {
             // Carregar o novo arquivo FXML
-            FXMLLoader loader = new FXMLLoader(TelaLoginControlador.class.getResource("telaEditar.fxml"));
+            FXMLLoader loader = new FXMLLoader(TelaLoginControlador.class.getResource("telaPerfil.fxml"));
             AnchorPane novaTela = loader.load();
             
             // Obter a cena atual e definir a nova tela como seu conteúdo
-            Stage stage = (Stage) linkParaTelaFeed.getScene().getWindow();
+            Stage stage = (Stage) hyperLinkTelaPerfil.getScene().getWindow();
             Scene novaCena = new Scene(novaTela);
             stage.setScene(novaCena);
 
@@ -86,4 +87,5 @@ public class TelaPerfilControlador {
             e.printStackTrace();
         }
     }
+
 }
