6fb7428751662c25c34e4ce3eaf8baa66fd87417
Uslipe <felipe.pereira.nave@gmail.com>
2024-09-12 07:56:33 -0300

    feat: Hover effect dos links da tela feed

---
+++ saboreando/src/main/java/com/saboreando/TelaFeedControlador.java
@@ -0,0 +1,30 @@
+package com.saboreando;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+
+public class TelaFeedControlador {
+    @FXML
+    private HBox hboxCriar;
+
+    @FXML
+    private HBox hboxFeed;
+
+    @FXML
+    private HBox hboxPerfil;
+
+    @FXML
+    private void initialize(){
+        //Hover effect do menu (BOTÃO FEED)
+        hboxFeed.setOnMouseEntered(event -> hboxFeed.setStyle("-fx-background-color: #f7b9cd; -fx-background-radius: 24"));
+        hboxFeed.setOnMouseExited(event -> hboxFeed.setStyle("-fx-background-color: transparent;"));
+
+        //Hover effect do menu (BOTÃO CRIAR)
+        hboxCriar.setOnMouseEntered(event -> hboxCriar.setStyle("-fx-background-color: #f7b9cd; -fx-background-radius: 24"));
+        hboxCriar.setOnMouseExited(event -> hboxCriar.setStyle("-fx-background-color: transparent;"));
+
+        //Hover effect do menu (BOTÃO PERFIL)
+        hboxPerfil.setOnMouseEntered(event -> hboxPerfil.setStyle("-fx-background-color: #f7b9cd; -fx-background-radius: 24"));
+        hboxPerfil.setOnMouseExited(event -> hboxPerfil.setStyle("-fx-background-color: transparent;"));
+    }
+}
