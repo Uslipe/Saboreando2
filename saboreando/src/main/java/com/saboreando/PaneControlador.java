8b707d87f77b58412989aa70c20914a4881bec99
Uslipe <felipe.pereira.nave@gmail.com>
2024-09-15 18:24:48 -0300

    feat: Tela postagem criada e link para ela a partir das postagens

--- saboreando/src/main/java/com/saboreando/PaneControlador.java
+++ saboreando/src/main/java/com/saboreando/PaneControlador.java
@@ -1,9 +1,19 @@
 package com.saboreando;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class PaneControlador {
+
+    @FXML
+    private Hyperlink hyperLinkTelaPostagem;
+
     @FXML
     private Label tituloPane;
 
@@ -14,5 +24,22 @@ public class PaneControlador {
     public void setData(String titulo, String username){
         tituloPane.setText(titulo);
         usernamePane.setText(username);
+    }
+
+    @FXML
+    public void HyperlinkActionTelaCriar(@SuppressWarnings("exports") ActionEvent event) {
+        try {
+            // Carregar o novo arquivo FXML
+            FXMLLoader loader = new FXMLLoader(TelaLoginControlador.class.getResource("telaPostagem.fxml"));
+            AnchorPane novaTela = loader.load();
+            
+            // Obter a cena atual e definir a nova tela como seu conteúdo
+            Stage stage = (Stage) hyperLinkTelaPostagem.getScene().getWindow();
+            Scene novaCena = new Scene(novaTela);
+            stage.setScene(novaCena);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
